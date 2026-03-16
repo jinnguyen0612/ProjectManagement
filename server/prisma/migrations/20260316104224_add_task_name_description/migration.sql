337a85ab072ea9a95800f114758f72203902cb46
Jin Nguyen <hoangha0612.work@gmail.com>
2026-03-16 18:18:56 +0700

    delete migration

--- server/prisma/migrations/20260316104224_add_task_name_description/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "tasks" ADD COLUMN "description" TEXT,
-ADD COLUMN "name" VARCHAR(500) NOT NULL DEFAULT 'Untitled Task';
-
--- Remove default after backfill (optional — keeps schema clean)
-ALTER TABLE "tasks" ALTER COLUMN "name" DROP DEFAULT;
--- server/prisma/migrations/20260316104943_add_task_is_completed/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "tasks" ADD COLUMN     "is_completed" BOOLEAN NOT NULL DEFAULT false;
