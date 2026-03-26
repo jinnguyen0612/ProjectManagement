047d0252333f775a68ea632d7c4bb2de83e89f1e
Jin Nguyen <hoangha0612.work@gmail.com>
2026-03-27 01:26:18 +0700

    fix setting FE

--- client/lib/api/index.ts
+++
@@ -1,7 +0,0 @@
-import apiClient from "./client";
-import { setupInterceptors } from "./interceptors";
-
-setupInterceptors();
-
-export { getAccessToken, getRefreshToken, setTokens, clearTokens } from "./interceptors";
-export default apiClient;
